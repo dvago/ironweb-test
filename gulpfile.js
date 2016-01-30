05ce3d03e87a303ad8ff4ff3bf04c33a9bd24b01
davide_vago <pigroz@gmail.com>
2016-01-30 19:40:26 +0000

    added uglify js and minify css

--- gulpfile.js
+++ gulpfile.js
@@ -36,9 +36,22 @@ gulp.task('style', function () {
     .pipe(gulp.dest('./public/css/'));
 });
 
+gulp.task('uglify', function() {
+  gulp.src('public/js/*.js')
+    .pipe($.uglify())
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('public/css/*.css')
+    .pipe($.minifyCss())
+    .pipe(gulp.dest('dist/css'));
+});
+
 gulp.task('watch', function () {
   gulp.watch('app/**/*.js', ['browserify'])
   gulp.watch(config.sassPath + '/**/*.scss', ['style'])
 })
 
 gulp.task('default', ['connect', 'move-images', 'watch']);
+gulp.task('build', ['uglify', 'minify-css']);
